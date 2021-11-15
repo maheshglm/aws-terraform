8119721f320a7f0f015d73ad66c815088c7347f5
maheshglm <maheshglm@gmail.com>
2021-11-15 23:13:37 +0800

    Updated docker template

--- learn-terraform-docker-container/main.tf
+++ learn-terraform-docker-container/main.tf
@@ -14,12 +14,12 @@ resource "docker_image" "nginx" {
     keep_locally = false
 }
 
-# resource "docker_container" "nginx" {
-#     image = docker_image.nginx.latest
-#     name = "tutorial"
+resource "docker_container" "nginx" {
+    image = docker_image.nginx.name
+    name = "tutorial"
 
-#     ports {
-#         internal = 80
-#         external = 8080
-#     }
-# }
+    ports {
+        internal = 80
+        external = 8080
+    }
+}
